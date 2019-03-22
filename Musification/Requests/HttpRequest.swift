4c9623c11c4cd8bab2924df3a623b84613518ffd
Rahil Patel <rahilpatel99@gmail.com>
2019-03-22 01:44:57 -0400

    made classes for genre http requests

---
+++ Musification/Requests/HttpRequest.swift
@@ -0,0 +1,54 @@
+//
+//  HttpRequest.swift
+//  Musification
+//
+//  Created by Rahil Patel on 3/21/19.
+//  Copyright © 2019 RahilPatel. All rights reserved.
+//
+
+import Foundation
+
+class HttpRequest {
+    static func makeGetRequest(urlString: String, success: @escaping (_ data: [String:Any]) -> Void, fail: @escaping (_ error: Error) -> Void) {
+        if let dbURL = URL(string: urlString) {
+            var dbRequest = URLRequest(url: dbURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 60)
+            dbRequest.httpMethod = "GET"
+            dbRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            let task = URLSession.shared.dataTask(with: dbRequest) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                        if let data = json as? [String:Any] {
+                            success(data)
+                        } else {
+                            fail(CustomError("JSON data was not an dictionary of [String:Any]"))
+                        }
+                    } catch {
+                        fail(error)
+                    }
+                } else {
+                    fail(CustomError("data is null"))
+                }
+            }
+            
+            task.resume()
+            
+        } else {
+            fail(CustomError("Bad URL string"))
+        }
+    }
+}
+
+struct CustomError : LocalizedError {
+    var errorDescription: String? { return mMsg }
+    var failureReason: String? { return mMsg }
+    var recoverySuggestion: String? { return "" }
+    var helpAnchor: String? { return "" }
+    
+    private var mMsg : String
+    
+    init(_ description: String)
+    {
+        mMsg = description
+    }
+}
---
+++ Musification/Requests/MusicRequest.swift
@@ -0,0 +1,46 @@
+//
+//  MusicRequest.swift
+//  Musification
+//
+//  Created by Rahil Patel on 3/21/19.
+//  Copyright © 2019 RahilPatel. All rights reserved.
+//
+
+import Foundation
+
+class MusicRequest : HttpRequest {
+    static let rootDbPath = "https://api.music.apple.com/v1/catalog/"
+    // can add functionality later to change based on locality
+    static let storefront = "us"
+    static func getGenres(success: @escaping (_ data: [String]) -> Void, fail: @escaping (_ error: Error) -> Void) {
+        let urlString = rootDbPath + storefront + "/genres"
+        super.makeGetRequest(urlString: urlString, success: { (data) in
+            processGenreData(data: data, success: { (genres) in
+                success(genres)
+            }, fail: { (error) in
+                fail(error)
+            })
+        }) { (error) in
+            fail(error)
+        }
+    }
+    private static func processGenreData(data: [String:Any], success: @escaping (_ data: [String]) -> Void, fail: @escaping (_ error: Error) -> Void) {
+        if let allGenreData = data["data"] as? [[String:Any]] {
+            var genres: [String] = []
+            for genreData in allGenreData {
+                if let genreAttribute = genreData["attributes"] as? [String:Any] {
+                    if let genreName = genreAttribute["name"] as? String {
+                        genres.append(genreName)
+                    } else {
+                        fail(CustomError("Genre attribute does not have a name or name is not a String."))
+                    }
+                } else {
+                    fail(CustomError("Genre data does not have attributes or attributes is not a String:Any pair."))
+                }
+            }
+            success(genres)
+        } else {
+            fail(CustomError("The data does not have \"data\" or data is not an array of String:Any pairs"))
+        }
+    }
+}
