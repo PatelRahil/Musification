88fccd13ffdc86be897789fc9f70794db39bd23c
Rahil Patel <rahilpatel99@gmail.com>
2019-03-24 18:10:47 -0400

    added FollowArtistVC and segues

---
+++ Musification/Controllers/FollowArtistVC.swift
@@ -0,0 +1,16 @@
+//
+//  FollowArtistVC.swift
+//  Musification
+//
+//  Created by Rahil Patel on 3/24/19.
+//  Copyright © 2019 RahilPatel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FollowArtistVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
