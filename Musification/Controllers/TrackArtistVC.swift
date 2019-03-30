7ab2f6f5d180d06f2ae028c0e3d1cd5fec7a611a
Rahil Patel <rahilpatel99@gmail.com>
2019-03-30 07:59:47 -0400

    recreated and renamed FollowArtistVC to TrackArtistVC

---
+++ Musification/Controllers/TrackArtistVC.swift
@@ -0,0 +1,14 @@
+//
+//  TrackArtistVC.swift
+//  Musification
+//
+//  Created by Rahil Patel on 3/30/19.
+//  Copyright © 2019 RahilPatel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackArtistVC: UIViewController {
+    
+}
